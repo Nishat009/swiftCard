67c90c1201f390b2284b7486c1efc79493baa9db
prantik <prantik>
2026-02-16 16:47:20 +0600

    working on the new all product page

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const productContainer = document.getElementById("product-container");
-
+const categoriesContainer = document.getElementById("category-list");
 async function fetchProducts() {
     try {
         const res = await fetch("https://fakestoreapi.com/products");
         const products = await res.json();
-        console.log(products);
-        
-        productContainer.innerHTML = products.map(product => `
+        productContainer.innerHTML = products.slice(0, 3).map(product => `
             <div  class="card shadow rounded-md border border-gray-50">
                         <!-- Product Image -->
                         <figure class="px-6 pt-6 bg-gray-100">
                             <img src=${product.image}
-                                alt=${product.title} class="rounded-lg p-5 object-cover h-40" />
+                                alt=${product.title} class="rounded-lg p-5 object-cover h-56" />
                         </figure>
                         <!-- Card Body -->
-                        <div class="card-body pt-4">
+                        <div class="card-body border-0 pt-4">
                             <div class="flex items-center justify-between">
                                 <span class="badge badge-soft badge-primary text-xs px-3 py-2">
                                     ${product.category}
@@ -30,11 +28,11 @@ async function fetchProducts() {
                             </h2>
                             <p class="text-xl font-bold text-gray-900">$${product.price}</p>
                             <div class="grid grid-cols-2 gap-4 mt-2">
-                                <button class="btn btn-outline flex items-center py-2 gap-2">
+                                <button class="btn border border-gray-300 flex items-center py-2 gap-2">
                                     <i class="fa-solid fa-eye"></i> Details
                                 </button>
 
-                                <button class="btn btn-primary flex items-center py-2 gap-2">
+                                <button class="btn btn-primary border-0 flex items-center py-2 gap-2">
                                     <i class="fa-solid fa-cart-shopping"></i> Add
                                 </button>
                             </div>
@@ -48,3 +46,26 @@ async function fetchProducts() {
     }
 }
 fetchProducts();
+
+async function fetchCategories() {
+    try {
+        const res = await fetch("https://fakestoreapi.com/products/categories");
+        const categories = await res.json();
+
+        categoriesContainer.innerHTML = `
+            <a role="button" class="btn rounded-full bg-indigo-600 text-white border-none">
+                All
+            </a>
+            ${categories.map(category => `
+                <a role="button"
+                   class="btn rounded-full hover:bg-indigo-600 border-indigo-600 hover:text-white text-indigo-600 hover:border-none">
+                   ${category}
+                </a>
+            `).join("")}
+        `;
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+    }    
+}
+
+fetchCategories();
